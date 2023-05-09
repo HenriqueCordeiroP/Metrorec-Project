f535b1f3547eccca3c662ae5a7ac9020ecb67787
Henrique Cordeiro <h.cordeirop@gmail.com>
2023-05-09 11:47:25 -0300

    needs atomic counter

--- metrorec-runner.c
+++ metrorec-runner.c
@@ -1,4 +1,5 @@
-// eu
+// hendrich
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,10 +7,10 @@
 
 #include "metrorec.c"
 
-void *passageiros_thread(void *arg)
-{
-    struct estacao *estacao = (struct estacao *) arg;
+void *passageiros_thread(void *arg) {
+    struct estacao *estacao = (struct estacao *)arg;
     estacao_espera_pelo_vagao(estacao);
+    estacao_embarque(estacao);
     return NULL;
 }
 
@@ -18,237 +19,51 @@ struct vagao_args {
     int assentos_livres;
 };
 
-void *vagao_thread(void *args)
-{
-    struct vagao_args *vargs = (struct vagao_args *) args;
-    estacao_preecher_vagao(vargs->estacao, vargs->assentos_livres);
+void *vagao_thread(void *args) {
+    struct vagao_args *vargs = (struct vagao_args *)args;
+    estacao_preencher_vagao(vargs->estacao, vargs->assentos_livres);
     return NULL;
 }
 
-int main(void)
-{
+void run_test(int num_passageiros, int num_assentos) {
+    struct estacao estacao;
+    estacao_init(&estacao);
 
-            // criação da estação
-            struct estacao estacao;
-            estacao_init(&estacao);
-            printf("Bem vindo ao METROREC.\n");
+    pthread_t passageiros[num_passageiros];
 
-            // definição da quantidade de passageiros
-            int numPassageiros = 12;
-            pthread_t threadsPassageiros[numPassageiros];
-            printf("Há %d passageiros na plataforma.\n", numPassageiros);
+    for (int i = 0; i < num_passageiros; i++) {
+        pthread_create(&passageiros[i], NULL, passageiros_thread, &estacao);
+    }
 
-            for (int i = 0 ; i < numPassageiros ; i++) {
-                estacao.id = i+1;
-                pthread_create(&threadsPassageiros[i], NULL, passageiros_thread, &estacao);
-                printf("Passageiro [%d] criado.\n", estacao.id);
-            }
-        
-            // variáveis usadas no loop
-            pthread_t vagao;
-            struct vagao_args vargs;
-            int restante = numPassageiros;
-            int reap;
-            int contVagao = 0;
-            // definição da struct
+    int num_passageiros_embarcados = 0;
 
-            while(restante != 0){
-                vargs.estacao = &estacao;
-                vargs.assentos_livres = 5;
-                // lançamento do vagão
-                pthread_create(&vagao, NULL, vagao_thread, &vargs);
-                printf("Um vagão está a caminho =D\n");
-                contVagao++;
-                // definição de quantos entram no vagão
-                if(restante > vargs.assentos_livres){
-                    reap = vargs.assentos_livres;
-                } else{
-                    reap = restante;
-                }
+    while (num_passageiros_embarcados < num_passageiros) {
+        struct vagao_args vargs;
+        vargs.estacao = &estacao;
+        vargs.assentos_livres = num_assentos;
 
-                // atualização dos restantes na plataforma
-                restante -= reap;
+        pthread_t vagao;
+        pthread_create(&vagao, NULL, vagao_thread, &vargs);
+        pthread_join(vagao, NULL);
 
-                for(int j = 0 ; j < reap ; j ++){
-                    estacao_embarque(&estacao);
-                }
-            }
-            printf("Todos os passageiros foram embarcados! (%d)\n", restante);
-            printf("Vagões utilizados: %d\n", contVagao);
-        return 0;
+        num_passageiros_embarcados += num_assentos;
+        if (num_passageiros_embarcados > num_passageiros) {
+            num_passageiros_embarcados = num_passageiros;
+        }
+    }
+
+    for (int i = 0; i < num_passageiros; i++) {
+        printf("Pre Join");
+        pthread_join(passageiros[i], NULL);
+    }
 }
 
-// falk
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <pthread.h>
+int main(void) {
+    // Execute a função 'run_test' com diferentes valores para os testes
+    run_test(12, 5);
+    // run_test(10, 3);
+    // run_test(6, 4);
+    // run_test(100, 25);
 
-// #include "metrorec.c"
-
-// void *passageiros_thread(void *arg)
-// {
-//     struct estacao *estacao = (struct estacao *) arg;
-//     estacao_espera_pelo_vagao(estacao);
-//     printf("Passageiro embarcou\n");
-//     return NULL;
-// }
-
-// struct vagao_args {
-//     struct estacao *estacao;
-//     int assentos_livres;
-// };
-
-// void *vagao_thread(void *args)
-// {
-//     struct vagao_args *vargs = (struct vagao_args *) args;
-//     estacao_preecher_vagao(vargs->estacao, vargs->assentos_livres);
-//     printf("Vagão preenchido\n");
-//     return NULL;
-// }
-
-// int main(void)
-// {
-//     struct estacao estacao;
-//     estacao_init(&estacao);
-
-//     int num_passageiros = 11;
-//     pthread_t passageiros[num_passageiros];
-
-//     for (int i = 0; i < num_passageiros; i++) {
-//         pthread_create(&passageiros[i], NULL, passageiros_thread, &estacao);
-//         printf("Passageiro %d criado\n", i);
-//     }
-
-//     int num_assentos = 3;
-//     int num_passageiros_embarcados = 0;
-//     int cont=0;
-
-//     while (num_passageiros_embarcados < num_passageiros) {
-//         struct vagao_args vargs;
-//         vargs.estacao = &estacao;
-//         vargs.assentos_livres = num_assentos;
-
-//         pthread_t vagao;
-//         pthread_create(&vagao, NULL, vagao_thread, &vargs);
-//         printf("Vagão criado com %d assentos livres\n", num_assentos);
-//         cont++;
-
-//         int num_passageiros_a_reap = num_passageiros - num_passageiros_embarcados;
-//         if (num_passageiros_a_reap > num_assentos) {
-//             num_passageiros_a_reap = num_assentos;
-//         }
-
-//         for (int i = 0; i < num_passageiros_a_reap; i++) {
-//             estacao_embarque(&estacao);
-//             num_passageiros_embarcados++;
-//             printf("Passageiro embarcou no vagão\n");
-//         }
-
-//         printf("Todos os passageiros embarcados no vagão\n");
-//     }
-//     printf("total de vagoes: %d\n",cont);
-//     printf("Todos os passageiros foram transportados\n");
-
-//     return 0;
-// }
-
-// fonta
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <pthread.h>
-
-// #include "metrorec.c"
-
-// void *passageiros_thread(void *arg)
-// {
-//     struct estacao *estacao = (struct estacao *)arg;
-//     estacao_espera_pelo_vagao(estacao);
-//     return NULL;
-// }
-
-// struct vagao_args
-// {
-//     struct estacao *estacao;
-//     int assentos_livres;
-// };
-
-// void *vagao_thread(void *args)
-// {
-//     struct vagao_args *vargs = (struct vagao_args *)args;
-//     estacao_preecher_vagao(vargs->estacao, vargs->assentos_livres);
-//     return NULL;
-// }
-
-// int main(void)
-// {
-
-//     // create one station
-//     struct estacao station;
-//     estacao_init(&station);
-
-//     //
-//     // create a number of passengers
-//     // each passenger is a thread
-//     int numPassengers = 10;
-//     pthread_t passengerThreads[numPassengers];
-//     for (int i = 0; i < numPassengers; i++)
-//     {
-//         pthread_create(&passengerThreads[i], NULL, passageiros_thread, (void *)&station);
-//         printf("Creating thread %d!\n", i + 1);
-//     }
-
-//     int seatsPerCar = 3, cont = 0;
-//     pthread_t carThread;
-
-//     // loop to create as many car as necessary to board all passengers
-//     while (numPassengers > 0)
-//     {
-//         printf("1\n");
-//         // Criar uma nova estrutura carArgs para cada iteração do loop
-//         struct vagao_args *carArgs = malloc(sizeof(struct vagao_args));
-//         carArgs->estacao = &station;
-//         carArgs->assentos_livres = seatsPerCar;
-
-//         printf("2\n");
-//         // create only one car with a number of free seats,this car is associated to a thread
-//         pthread_create(&carThread, NULL, vagao_thread, (void *)carArgs);
-
-//         /* if (numPassengers == 0)
-//         {
-//             break;
-//         } */
-
-//         printf("3\n");
-//         // define the number of passenger to reap
-//         int numPassengersToReap = (numPassengers > seatsPerCar) ? seatsPerCar : numPassengers;
-//         numPassengers -= numPassengersToReap;
-//         // for each thread associated to a passenger that finished
-
-//         printf("4\n");
-//         for (int i = 0; i < numPassengersToReap; i++)
-//         {
-//             // call estacao_embarque function to let the car know that the passenger is on board
-//             estacao_embarque(&station);
-//         }
-//         printf("5\n");
-
-//         if (numPassengers == 0)
-//         {
-//             for (int i = 0; i < cont; i++)
-//             {
-//                 free(carArgs);
-//             }
-//         }
-
-//         printf("6\n");
-//     }
-//     printf("7\n");
-//     pthread_mutex_destroy(&station.mutex);
-//     /*     pthread_cond_destroy(&station.condVagao);
-//         pthread_cond_destroy(&station.condChegou);
-//         pthread_cond_destroy(&station.condEmbarque);
-//         pthread_cond_destroy(&station.condPegouLock); */
-//     printf("8\n");
-// }
+    return 0;
+}
--- metrorec.c
+++ metrorec.c
@@ -1,13 +1,16 @@
 #include <pthread.h>
+#include <stdio.h>
 
 struct estacao {
-    int contPassageiros;
     pthread_mutex_t mutex;
     pthread_cond_t cond;
-    int assentosLivres;
     pthread_cond_t condVagao;
+    int contPassageiros;
+    int assentosLivres;
+    int alocando;
     int id; 
 };
+int i = 1;
 void estacao_init(struct estacao *estacao) {
     pthread_mutex_init(&estacao->mutex, NULL);
     pthread_cond_init(&estacao->cond, NULL);
@@ -15,26 +18,29 @@ void estacao_init(struct estacao *estacao) {
     estacao->contPassageiros = 0;
 }
 
-void estacao_preecher_vagao(struct estacao * estacao, int assentos) { // 1 thread
+void estacao_preencher_vagao(struct estacao * estacao, int assentos) { // 1 thread
     pthread_mutex_lock(&estacao->mutex);
+    // printf("PV - Passageiros - %d\n", estacao->contPassageiros);
     estacao->assentosLivres = assentos;
+    estacao->alocando = assentos;
     pthread_cond_broadcast(&estacao->cond);
     pthread_cond_wait(&estacao->condVagao, &estacao->mutex);
     printf("Vagão indo embora\n");
     pthread_mutex_unlock(&estacao->mutex);
 }
 
-void estacao_espera_pelo_vagao(struct estacao * estacao) {
+void estacao_espera_pelo_vagao(struct estacao * estacao) { // passou dessa ta valendo
     pthread_mutex_lock(&estacao->mutex);
     estacao->contPassageiros++;
-    printf("Passageiros: %d \n", estacao->contPassageiros);
-    pthread_cond_wait(&estacao->cond, &estacao->mutex);
+    // printf("Passageiros: %d \n", estacao->contPassageiros);
+    pthread_cond_wait(&estacao->cond, &estacao->mutex); // ele respeita o mutex na volta?
     while(estacao->contPassageiros > 0){
         if(estacao->assentosLivres > 0 && estacao->contPassageiros > 0){
+            printf("EPV - Passageiros - %d\n", estacao->contPassageiros);
+            estacao->contPassageiros--; // erro
             estacao->assentosLivres--;
-            estacao->contPassageiros--;
         } else{
-                pthread_cond_wait(&estacao->cond, &estacao->mutex);
+            pthread_cond_wait(&estacao->cond, &estacao->mutex);
         }
     }
     pthread_mutex_unlock(&estacao->mutex);
@@ -42,12 +48,18 @@ void estacao_espera_pelo_vagao(struct estacao * estacao) {
 
 void estacao_embarque(struct estacao * estacao) {
     pthread_mutex_lock(&estacao->mutex);
-    printf("Embarcado: %d\n", estacao->id);
-    if(estacao->assentosLivres == 0){
-        pthread_cond_signal(&estacao->condVagao);
+    // printf("E - Passageiros %d\n",estacao->contPassageiros);
+    // printf("E - Livres - %d\n", estacao->assentosLivres);
+    // estacao->assentosLivres--; // faz sentido?
+    estacao->alocando--;
+    // printf("E - Embarcado: %d\n", i++);
+    // printf("E - Passageiros - %d\n", estacao->contPassageiros);
+    if(estacao->alocando ==  0 || estacao->contPassageiros == 0){
+        pthread_cond_signal(&estacao->condVagao);   
     }
     pthread_mutex_unlock(&estacao->mutex);
 }
+// embarque so chama quando a thread do passageiro termina
 
 // usar array pra ocupar lugares? 
     // melhor do que variável contadora de assentos?
